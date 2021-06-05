64010ae95f0e4aa23cb16598a2e6cc07e876b227
Paul Akulich <akulich.p@gmail.com>
2021-06-06 01:21:43 +0400

    Refactored utils for multiple inheritance; added tests

--- src/utilities/implementations/classes/multipleInheritance.test.ts
+++ src/utilities/implementations/classes/multipleInheritance.test.ts
@@ -1,40 +1,391 @@
-import {applyMixins} from 'src/utilities/implementations/classes/multipleInheritance';
+import {Constructor} from 'src/interfaces/classes';
+import {
+  applyMixinsMethodInheritance,
+  applyMixinsPropsAndMethodsInheritance,
+} from 'src/utilities/implementations/classes/multipleInheritance';
 
-describe('applyMixins', () => {
-  it('Should extend class with other abstractClasses', () => {
-    const aValue = 'a';
-    const bValue = 'b';
-    const cValue = 'c';
-    class TestClassExtend {
-      a = aValue;
-      getA() {
-        return this.a;
+describe('multipleInheritance', () => {
+  describe('applyMixinsMethodInheritance', () => {
+    it('Should not change an original array of constructors passed', () => {
+      const aValue = 'a';
+      const bValue = 'b';
+      const cValue = 'c';
+      class TestClassExtend {
+        a = aValue;
+        smth = aValue;
+        getA() {
+          return this.a;
+        }
+        getSmth() {
+          return this.smth;
+        }
       }
-    }
-    class TestClassExtendSecond {
-      b = bValue;
-      getB() {
-        return this.b;
+      class TestClassExtendSecond {
+        b = bValue;
+        smth = bValue;
+        getB() {
+          return this.b;
+        }
+        getSmth() {
+          return this.b;
+        }
       }
-    }
-    class TestClass {
-      c = cValue;
-      getC() {
-        return this.c;
+      class TestClass {
+        get c() {
+          return cValue;
+        }
+        smth = cValue;
+        getC() {
+          return this.c;
+        }
+        getSmth() {
+          return this.smth;
+        }
       }
-    }
 
-    interface TestClass extends TestClassExtend, TestClassExtendSecond {}
+      const constructorsToAppply: [
+        Constructor<TestClassExtend>,
+        Constructor<TestClassExtendSecond>
+      ] = [TestClassExtend, TestClassExtendSecond];
 
-    applyMixins(TestClass, [TestClassExtend, TestClassExtendSecond]);
+      class NewTestClass extends applyMixinsMethodInheritance(
+        TestClass,
+        constructorsToAppply
+      ) {}
 
-    const testInstance = new TestClass();
+      new NewTestClass();
+      expect(constructorsToAppply[0]).toBe(TestClassExtend);
+      expect(constructorsToAppply[1]).toBe(TestClassExtendSecond);
+    });
+    it('Should inherit only methods from classes passed - methods of inherited classes with references to "this"', () => {
+      const aValue = 'a';
+      const bValue = 'b';
+      const cValue = 'c';
+      class TestClassExtend {
+        a = aValue;
+        smth = aValue;
+        getA() {
+          return this.a;
+        }
+        getSmth() {
+          return this.smth;
+        }
+      }
+      class TestClassExtendSecond {
+        b = bValue;
+        smth = bValue;
+        getB() {
+          return this.b;
+        }
+        getSmth() {
+          return this.b;
+        }
+      }
+      class TestClass {
+        get c() {
+          return cValue;
+        }
+        smth = cValue;
+        getC() {
+          return this.c;
+        }
+        getSmth() {
+          return this.smth;
+        }
+      }
 
-    expect(testInstance.a).toBe(aValue);
-    expect(testInstance.getA()).toBe(aValue);
-    expect(testInstance.b).toBe(bValue);
-    expect(testInstance.getB()).toBe(bValue);
-    expect(testInstance.c).toBe(cValue);
-    expect(testInstance.getC()).toBe(cValue);
+      class NewTestClass extends applyMixinsMethodInheritance(TestClass, [
+        TestClassExtend,
+        TestClassExtendSecond,
+      ]) {}
+
+      const testInstance = new NewTestClass();
+
+      expect((testInstance as any).a).toBeUndefined();
+      expect(testInstance.getA()).toBeUndefined();
+      expect((testInstance as any).b).toBeUndefined();
+      expect(testInstance.getB()).toBeUndefined();
+      expect(testInstance.c).toBe(cValue);
+      expect(testInstance.getC()).toBe(cValue);
+      expect(testInstance.smth).toBe(cValue);
+      expect(testInstance.getSmth()).toBe(cValue);
+    });
+    it('Should inherit only methods from classes passed in the same order - methods of inherited classes without references to "this"', () => {
+      const aValue = 'a';
+      const bValue = 'b';
+      const cValue = 'c';
+      class TestClassExtend {
+        a = aValue;
+        smth = aValue;
+        getA() {
+          return aValue;
+        }
+        getSmth() {
+          return aValue;
+        }
+      }
+      class TestClassExtendSecond {
+        b = bValue;
+        smth = bValue;
+        c = bValue;
+        getA() {
+          return bValue;
+        }
+        getB() {
+          return bValue;
+        }
+        getSmth() {
+          return bValue;
+        }
+      }
+      class TestClass {
+        c = cValue;
+        smth = cValue;
+        getC() {
+          return cValue;
+        }
+        getSmth() {
+          return cValue;
+        }
+      }
+
+      class NewTestClass extends applyMixinsMethodInheritance(TestClass, [
+        TestClassExtend,
+        TestClassExtendSecond,
+      ]) {}
+
+      const testInstance = new NewTestClass();
+
+      expect((testInstance as any).a).toBeUndefined();
+      expect(testInstance.getA()).toBe(aValue);
+      expect((testInstance as any).b).toBeUndefined();
+      expect(testInstance.getB()).toBe(bValue);
+      expect(testInstance.c).toBe(cValue);
+      expect(testInstance.getC()).toBe(cValue);
+      expect(testInstance.smth).toBe(cValue);
+      expect(testInstance.getSmth()).toBe(cValue);
+    });
+    it('Should inherit only methods from classes passed in the same order and exclude all getters and setters', () => {
+      const aValue = 'a';
+      const bValue = 'b';
+      const cValue = 'c';
+      const dValue = 'd';
+      const eValue = 'e';
+      class TestClassExtend {
+        a = aValue;
+        smth = aValue;
+        get d() {
+          return dValue;
+        }
+        set d(v) {
+          this.a = v;
+        }
+
+        getA() {
+          return aValue;
+        }
+        getSmth() {
+          return aValue;
+        }
+      }
+      class TestClassExtendSecond {
+        b = bValue;
+        smth = bValue;
+        c = bValue;
+        get e() {
+          return bValue;
+        }
+        getB() {
+          return bValue;
+        }
+        getSmth() {
+          return bValue;
+        }
+      }
+      class TestClass {
+        c = cValue;
+        smth = cValue;
+        get e() {
+          return eValue;
+        }
+        getC() {
+          return cValue;
+        }
+        getSmth() {
+          return cValue;
+        }
+      }
+
+      class NewTestClass extends applyMixinsMethodInheritance(TestClass, [
+        TestClassExtend,
+        TestClassExtendSecond,
+      ]) {}
+
+      const testInstance = new NewTestClass();
+
+      expect((testInstance as any).a).toBeUndefined();
+      expect(testInstance.getA()).toBe(aValue);
+      expect((testInstance as any).b).toBeUndefined();
+      expect(testInstance.getB()).toBe(bValue);
+      expect(testInstance.c).toBe(cValue);
+      expect(testInstance.getC()).toBe(cValue);
+      expect((testInstance as any).d).toBeUndefined();
+      expect(testInstance.e).toBe(eValue);
+      expect(testInstance.smth).toBe(cValue);
+      expect(testInstance.getSmth()).toBe(cValue);
+    });
+    it('Should inherit only methods from classes passed in the same order and include all getters and setters', () => {
+      const aValue = 'a';
+      const bValue = 'b';
+      const cValue = 'c';
+      const dValue = 'd';
+      const eValue = 'e';
+      class TestClassExtend {
+        a = aValue;
+        smth = aValue;
+        private __dValue = dValue;
+        get d() {
+          return this.__dValue;
+        }
+        set d(v) {
+          this.__dValue = v;
+        }
+
+        getA() {
+          return aValue;
+        }
+        getSmth() {
+          return aValue;
+        }
+      }
+      class TestClassExtendSecond {
+        b = bValue;
+        smth = bValue;
+        c = bValue;
+        get e() {
+          return bValue;
+        }
+        getB() {
+          return bValue;
+        }
+        getSmth() {
+          return bValue;
+        }
+      }
+      class TestClass {
+        c = cValue;
+        smth = cValue;
+        get e() {
+          return eValue;
+        }
+        getC() {
+          return cValue;
+        }
+        getSmth() {
+          return cValue;
+        }
+      }
+
+      class NewTestClass extends applyMixinsMethodInheritance(TestClass, [
+        TestClassExtend,
+        TestClassExtendSecond,
+      ]) {}
+
+      const testInstance = new NewTestClass();
+
+      expect((testInstance as any).a).toBeUndefined();
+      expect(testInstance.getA()).toBe(aValue);
+      expect((testInstance as any).b).toBeUndefined();
+      expect(testInstance.getB()).toBe(bValue);
+      expect(testInstance.c).toBe(cValue);
+      expect(testInstance.getC()).toBe(cValue);
+      expect((testInstance as any).d).toBeUndefined();
+      expect(testInstance.e).toBe(eValue);
+      expect(testInstance.smth).toBe(cValue);
+      expect(testInstance.getSmth()).toBe(cValue);
+    });
+  });
+  describe('applyMixinsPropsAndMethodsInheritance', () => {
+    it('Should extend class with other abstractClasses', () => {
+      const aValue = 'a';
+      const bValue = 'b';
+      const cValue = 'c';
+      const dValue = 'd';
+      const eValue = 'e';
+      class TestClassExtend {
+        a = aValue;
+        smth = aValue;
+        get e() {
+          return aValue;
+        }
+        private __dValue = dValue;
+        get d() {
+          return this.__dValue;
+        }
+        set d(v) {
+          this.__dValue = v;
+        }
+        getA() {
+          return aValue;
+        }
+        getSmth() {
+          return aValue;
+        }
+      }
+      class TestClassExtendSecond {
+        b = bValue;
+        smth = bValue;
+        c = bValue;
+        get d() {
+          return bValue;
+        }
+        set d(v) {
+          return;
+        }
+        getB() {
+          return bValue;
+        }
+        getSmth() {
+          return bValue;
+        }
+      }
+      class TestClass {
+        c = cValue;
+        smth = cValue;
+        get e() {
+          return eValue;
+        }
+        getC() {
+          return cValue;
+        }
+        getSmth() {
+          return cValue;
+        }
+      }
+
+      class NewTestClass extends applyMixinsPropsAndMethodsInheritance(
+        TestClass,
+        TestClassExtend,
+        TestClassExtendSecond
+      ) {}
+
+      const testInstance = new NewTestClass();
+
+      expect(testInstance.a).toBe(aValue);
+      expect(testInstance.getA()).toBe(aValue);
+      expect(testInstance.b).toBe(bValue);
+      expect(testInstance.getB()).toBe(bValue);
+      expect(testInstance.c).toBe(cValue);
+      expect(testInstance.getC()).toBe(cValue);
+      expect(testInstance.e).toBe(eValue);
+      expect(testInstance.d).toBe(dValue);
+      expect(testInstance.smth).toBe(cValue);
+      expect(testInstance.getSmth()).toBe(cValue);
+
+      const changedDValue = '1';
+
+      testInstance.d = changedDValue;
+      expect(testInstance.d).toBe(changedDValue);
+    });
   });
 });
--- src/utilities/implementations/classes/multipleInheritance.ts
+++ src/utilities/implementations/classes/multipleInheritance.ts
@@ -1,17 +1,216 @@
 import {Constructor} from '../../../interfaces/classes';
+import {PickByValueType} from '../../../interfaces/common';
 
-export function applyMixins(
-  derivedCtor: Constructor,
-  constructors: Constructor[]
-) {
-  constructors.forEach(baseCtor => {
+import {copyOwnPropertiesIntoTarget} from '../objects';
+
+function applyConstructor<CTOR extends Constructor>(
+  constructor: CTOR,
+  args: ConstructorParameters<CTOR>
+): InstanceType<CTOR> {
+  // eslint-disable-next-line prefer-spread
+  const factoryFunction = constructor.bind.apply(constructor, args as any);
+  return new factoryFunction() as unknown as InstanceType<CTOR>;
+}
+
+export function applyMixinsMethodInheritance<
+  C extends Constructor,
+  C1 extends Constructor,
+  C2 extends Constructor
+>(
+  DerivedCtor: C,
+  constructors: [C1, C2]
+): Constructor<
+  InstanceType<C> &
+    PickByValueType<InstanceType<C1>, Function> &
+    PickByValueType<InstanceType<C2>, Function>,
+  ConstructorParameters<C>
+>;
+export function applyMixinsMethodInheritance<
+  C extends Constructor,
+  C1 extends Constructor,
+  C2 extends Constructor,
+  C3 extends Constructor
+>(
+  DerivedCtor: C,
+  constructors: [C1, C2, C3]
+): Constructor<
+  InstanceType<C> &
+    PickByValueType<InstanceType<C1>, Function> &
+    PickByValueType<InstanceType<C2>, Function> &
+    PickByValueType<InstanceType<C3>, Function>,
+  ConstructorParameters<C>
+>;
+export function applyMixinsMethodInheritance<
+  C extends Constructor,
+  C1 extends Constructor,
+  C2 extends Constructor,
+  C3 extends Constructor,
+  C4 extends Constructor
+>(
+  DerivedCtor: C,
+  constructors: [C1, C2, C3, C4]
+): Constructor<
+  InstanceType<C> &
+    PickByValueType<InstanceType<C1>, Function> &
+    PickByValueType<InstanceType<C2>, Function> &
+    PickByValueType<InstanceType<C3>, Function> &
+    PickByValueType<InstanceType<C4>, Function>,
+  ConstructorParameters<C>
+>;
+export function applyMixinsMethodInheritance<
+  C extends Constructor,
+  C1 extends Constructor,
+  C2 extends Constructor,
+  C3 extends Constructor,
+  C4 extends Constructor,
+  C5 extends Constructor
+>(
+  DerivedCtor: C,
+  constructors: [C1, C2, C3, C4, C5]
+): Constructor<
+  InstanceType<C> &
+    PickByValueType<InstanceType<C1>, Function> &
+    PickByValueType<InstanceType<C2>, Function> &
+    PickByValueType<InstanceType<C3>, Function> &
+    PickByValueType<InstanceType<C4>, Function> &
+    PickByValueType<InstanceType<C5>, Function>,
+  ConstructorParameters<C>
+>;
+export function applyMixinsMethodInheritance<C extends Constructor>(
+  DerivedCtor: C,
+  constructors: Constructor[],
+  options: {withGettersAndSetters?: boolean} = {withGettersAndSetters: false}
+): Constructor {
+  const {withGettersAndSetters} = options;
+  class Target extends DerivedCtor {}
+  [...constructors].reverse().forEach(baseCtor => {
     Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
+      if (
+        !withGettersAndSetters &&
+        typeof baseCtor.prototype[name] !== 'function'
+      ) {
+        return;
+      }
+      if (DerivedCtor.prototype[name]) {
+        // do not override a method it it's already exists
+        return;
+      }
       Object.defineProperty(
-        derivedCtor.prototype,
+        Target.prototype,
         name,
         Object.getOwnPropertyDescriptor(baseCtor.prototype, name) ||
           Object.create(null)
       );
     });
   });
+  return Target;
+}
+
+function applyMixinsMethodInheritanceWithGettersAndSetters(
+  DerivedCtor: Constructor,
+  constructors: Constructor[]
+): Constructor {
+  return (applyMixinsMethodInheritance as any)(DerivedCtor, constructors, {
+    withGettersAndSetters: true,
+  });
+}
+
+export function applyMixinsPropsAndMethodsInheritance<
+  C1 extends Constructor,
+  C2 extends Constructor
+>(
+  ctor1: C1,
+  ctor2: C2
+): Constructor<InstanceType<C2> & InstanceType<C1>, ConstructorParameters<C1>>;
+export function applyMixinsPropsAndMethodsInheritance<
+  C1 extends Constructor,
+  C2 extends Constructor,
+  C3 extends Constructor
+>(
+  ctor1: C1,
+  ctor2: C2,
+  ctor3: C3
+): Constructor<
+  InstanceType<C3> & InstanceType<C2> & InstanceType<C1>,
+  ConstructorParameters<C1>
+>;
+export function applyMixinsPropsAndMethodsInheritance<
+  C1 extends Constructor,
+  C2 extends Constructor,
+  C3 extends Constructor,
+  C4 extends Constructor
+>(
+  ctor1: C1,
+  ctor2: C2,
+  ctor3: C3,
+  ctor4: C4
+): Constructor<
+  InstanceType<C4> & InstanceType<C3> & InstanceType<C2> & InstanceType<C1>,
+  ConstructorParameters<C1>
+>;
+export function applyMixinsPropsAndMethodsInheritance<
+  C1 extends Constructor,
+  C2 extends Constructor,
+  C3 extends Constructor,
+  C4 extends Constructor,
+  C5 extends Constructor
+>(
+  ctor1: C1,
+  ctor2: C2,
+  ctor3: C3,
+  ctor4: C4,
+  ctor5: C5
+): Constructor<
+  InstanceType<C5> &
+    InstanceType<C4> &
+    InstanceType<C3> &
+    InstanceType<C2> &
+    InstanceType<C1>,
+  ConstructorParameters<C1>
+>;
+export function applyMixinsPropsAndMethodsInheritance<
+  C1 extends Constructor,
+  C2 extends Constructor,
+  C3 extends Constructor,
+  C4 extends Constructor,
+  C5 extends Constructor,
+  C6 extends Constructor
+>(
+  ctor1: C1,
+  ctor2: C2,
+  ctor3: C3,
+  ctor4: C4,
+  ctor5: C5,
+  ctor6: C6
+): Constructor<
+  InstanceType<C6> &
+    InstanceType<C5> &
+    InstanceType<C4> &
+    InstanceType<C3> &
+    InstanceType<C2> &
+    InstanceType<C1>,
+  ConstructorParameters<C1>
+>;
+export function applyMixinsPropsAndMethodsInheritance(
+  ...constructors: Constructor[]
+): Constructor {
+  class ClassWithMethodsInherited {}
+  const reversedConstructors = [...constructors].reverse();
+  const prototypeWithMethodsDerived =
+    applyMixinsMethodInheritanceWithGettersAndSetters(
+      ClassWithMethodsInherited,
+      constructors
+    ).prototype;
+
+  const derivedCtor = function <CTOR extends typeof constructors[0]>(
+    this: CTOR,
+    ...args: ConstructorParameters<CTOR>
+  ) {
+    reversedConstructors.forEach(baseCtor => {
+      const instance = applyConstructor(baseCtor, args);
+      copyOwnPropertiesIntoTarget(this, instance);
+    });
+    copyOwnPropertiesIntoTarget(this, prototypeWithMethodsDerived);
+  };
+  return derivedCtor as unknown as Constructor;
 }
