a35836c0839068b29644b9c1b604a1b3ff05b3c3
Paul Akulich <akulich.p@gmail.com>
2021-06-06 18:00:45 +0400

    refactored email value object validation

--- src/valueObjects/classes/emailValueObject.ts
+++ src/valueObjects/classes/emailValueObject.ts
@@ -1,15 +1,26 @@
 import {BaseValueObjectStringSerialization} from 'src/valueObjects/interfaces/abstractClasses/baseValueObject';
 import {IEmailVO} from '../interfaces/emailValueObject';
 
+function getEmailValidationRegexp(): RegExp {
+  // https://html.spec.whatwg.org/multipage/input.html#valid-e-mail-address
+  return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+}
+
 export class EmailVO
   extends BaseValueObjectStringSerialization<string>
   implements IEmailVO
 {
   protected _validate(): void {
+    const trimmedEmail = this.value.trim();
     if (
-      !/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
-        this.value
-      )
+      trimmedEmail.startsWith('.') ||
+      trimmedEmail.includes('.@') ||
+      trimmedEmail.includes('@.') ||
+      trimmedEmail.includes(' ') ||
+      trimmedEmail.includes('..') ||
+      !/@.*\./.test(trimmedEmail) ||
+      /@\d{3}\.\d{3}\.\d{3}\.\d{4,}/.test(trimmedEmail) ||
+      !getEmailValidationRegexp().test(trimmedEmail)
     ) {
       throw new Error('The email is not valid');
     }
--- src/valueObjects/classes/uuidValueObject.test.ts
+++ src/valueObjects/classes/uuidValueObject.test.ts
@@ -1,1 +1,34 @@
-// TODO
+import {UuidValueObject} from 'src/valueObjects/classes/uuidValueObject';
+
+const VALID_UUID_V4 = [
+  '2a17485a-e133-4424-a890-d7d3b6aa861e',
+
+  '64ed54e8-58f4-4cec-9f1a-dedd14981d76',
+
+  'd06a5aca-2581-4d3e-936f-431ee5382a28',
+
+  '64f4756a-dfa4-4654-ba67-3da3a75fca3b',
+];
+
+const INVALID_UUID_V4 = [
+  '2a17485a-e133-4424-a890-d7d3b6aa861',
+
+  '64ed54e8-58f4-4cec-9f1a-3333',
+
+  'd06a5aca-2581-4d3e-9999-431ee5382a212',
+  '',
+  0 as any,
+];
+
+describe('UuidValueObject', () => {
+  it.each(VALID_UUID_V4)('Should not throw for "%s"', validUUIDV4String => {
+    expect(() => {
+      new UuidValueObject(validUUIDV4String);
+    }).not.toThrow();
+  });
+  it.each(INVALID_UUID_V4)('Should throw for "%s"', invalidUUIDV4String => {
+    expect(() => {
+      new UuidValueObject(invalidUUIDV4String);
+    }).toThrow();
+  });
+});
