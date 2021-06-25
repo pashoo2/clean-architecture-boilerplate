f210663fe410b5b3a834091a6ca61f540f413412
Paul Akulich <akulich.p@gmail.com>
2021-06-26 01:01:52 +0400

    refactored fabric of domain entity events constructors

--- src/events/classes/fabricDomainEntityEventConstructor/fabricDomainEntityEventConstructor.test.ts
+++ src/events/classes/fabricDomainEntityEventConstructor/fabricDomainEntityEventConstructor.test.ts
@@ -1,71 +1,48 @@
-import {TBaseDomainEntityEventParameters} from 'src/events/classes/baseDomainEntityEvent/baseDomainEntityEvent';
+import {TBaseDomainEntityOrAggregateEventParameters} from 'src/events/classes/baseDomainEntityOrAggregateEvent/baseDomainEntityOrAggregateEvent';
+import {testBaseDomainEntityOrAggregateEvent} from 'src/events/classes/baseDomainEntityOrAggregateEvent/baseDomainEntityOrAggregateEvent.test';
 import {createDomainEntityEventConstructor} from 'src/events/classes/fabricDomainEntityEventConstructor/fabricDomainEntityEventConstructor';
 import {
-  MultipleIdentityValueObjectClassMock,
-  SimpleIdentityValueObjectClassMock,
-  UNIQUE_ENTITY_IDENTITY_MULTI_STUB,
-  UNIQUE_ENTITY_IDENTITY_SIMPLE_STUB,
-} from 'src/__mock__/valueObjects.mock';
+  DOMAIN_ENTITY_EVENT_CONSTRUCTOR_PARAMETERS_WITHOUT_PAYLOAD_MULTI_ENTITY_ID_STUB,
+  DOMAIN_ENTITY_EVENT_CONSTRUCTOR_PARAMETERS_WITHOUT_PAYLOAD_SIMPLE_ENTITY_ID_STUB,
+  DOMAIN_ENTITY_EVENT_CONSTRUCTOR_PARAMETERS_WITH_PAYLOAD_MULTI_ENTITY_ID_STUB,
+  DOMAIN_ENTITY_EVENT_CONSTRUCTOR_PARAMETERS_WITH_PAYLOAD_SIMPLE_ENTITY_ID_STUB,
+  EVENT_ENTITY_TYPE_STUB,
+  EVENT_NAME_STUB,
+  EVENT_TYPE_STUB,
+} from 'src/__mock__/domainEvents/constructorParameters.stub';
 
-const EVENT_IDENTITY_UNIQUE = 'EVENT_IDENTITY_UNIQUE' as const;
-const EVENT_NAME = 'EVENT_NAME' as const;
-const ENTITY_TYPE = 'ENTITY_TYPE' as const;
-
-describe('fabricDomainEntityEventConstructor', () => {
-  const parametersWithoutPayloadSimpleEntityId: TBaseDomainEntityEventParameters<
-    SimpleIdentityValueObjectClassMock,
-    undefined
-  > = {
-    id: EVENT_IDENTITY_UNIQUE,
-    metaVersion: 1,
-    entityId: new SimpleIdentityValueObjectClassMock(
-      UNIQUE_ENTITY_IDENTITY_SIMPLE_STUB
-    ),
-  };
-  const parametersWithoutPayloadMultiEntityId: TBaseDomainEntityEventParameters<
-    MultipleIdentityValueObjectClassMock,
-    undefined
-  > = {
-    ...parametersWithoutPayloadSimpleEntityId,
-    entityId: new MultipleIdentityValueObjectClassMock(
-      UNIQUE_ENTITY_IDENTITY_MULTI_STUB
-    ),
-  };
-  const parametersWithPayloadSimpleEntityId: TBaseDomainEntityEventParameters<
-    SimpleIdentityValueObjectClassMock,
-    {isPayload: boolean}
-  > = {
-    id: EVENT_IDENTITY_UNIQUE,
-    payload: {isPayload: true},
-    entityId: new SimpleIdentityValueObjectClassMock(
-      UNIQUE_ENTITY_IDENTITY_SIMPLE_STUB
-    ),
-  };
-  const parametersWithPayloadMultiEntityId: TBaseDomainEntityEventParameters<
-    MultipleIdentityValueObjectClassMock,
-    {isPayload: boolean}
-  > = {
-    ...parametersWithPayloadSimpleEntityId,
-    entityId: new MultipleIdentityValueObjectClassMock(
-      UNIQUE_ENTITY_IDENTITY_MULTI_STUB
-    ),
-  };
-
-  class DomainEventClassTest extends createDomainEntityEventConstructor<
-    typeof EVENT_NAME,
-    typeof ENTITY_TYPE,
-    any
-  >({
-    eventName: EVENT_NAME,
-    entityType: ENTITY_TYPE,
-  }) {}
-
+describe('Base domain entity event', () => {
   describe.each([
-    parametersWithoutPayloadSimpleEntityId,
-    parametersWithoutPayloadMultiEntityId,
-    parametersWithPayloadSimpleEntityId,
-    parametersWithPayloadMultiEntityId,
-  ])('Parameters %p', constructorParameters => {
-    
-  });
+    DOMAIN_ENTITY_EVENT_CONSTRUCTOR_PARAMETERS_WITHOUT_PAYLOAD_SIMPLE_ENTITY_ID_STUB,
+    DOMAIN_ENTITY_EVENT_CONSTRUCTOR_PARAMETERS_WITHOUT_PAYLOAD_MULTI_ENTITY_ID_STUB,
+    DOMAIN_ENTITY_EVENT_CONSTRUCTOR_PARAMETERS_WITH_PAYLOAD_SIMPLE_ENTITY_ID_STUB,
+    DOMAIN_ENTITY_EVENT_CONSTRUCTOR_PARAMETERS_WITH_PAYLOAD_MULTI_ENTITY_ID_STUB,
+  ])(
+    'Parameters %p',
+    (
+      constructorParameters: TBaseDomainEntityOrAggregateEventParameters<
+        any,
+        any
+      >
+    ) => {
+      class DomainEventClassTest extends createDomainEntityEventConstructor<
+        typeof EVENT_NAME_STUB,
+        typeof EVENT_ENTITY_TYPE_STUB,
+        any
+      >({
+        eventName: EVENT_NAME_STUB,
+        entityType: EVENT_ENTITY_TYPE_STUB,
+      }) {}
+      testBaseDomainEntityOrAggregateEvent({
+        constructorParameters,
+        entityType: EVENT_ENTITY_TYPE_STUB,
+        eventName: EVENT_NAME_STUB,
+        eventPayload: (constructorParameters as any).payload,
+        eventType: EVENT_TYPE_STUB,
+        getEntity() {
+          return new DomainEventClassTest(constructorParameters) as any;
+        },
+      });
+    }
+  );
 });
